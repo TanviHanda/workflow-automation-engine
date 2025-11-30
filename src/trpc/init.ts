a60d69391a9ba2481d063f8230361962f868a904
TanviHanda <tanvisona2012@gmail.com>
2025-11-30 09:48:24 +0530

    03:trpc-setup

---
+++ src/trpc/init.ts
@@ -0,0 +1,27 @@
+import { initTRPC } from '@trpc/server';
+import { cache } from 'react';
+export const createTRPCContext = cache(async () => {
+  /**
+   * @see: https://trpc.io/docs/server/context
+   */
+  return { userId: 'user_123' };
+});
+// Avoid exporting the entire t-object
+// since it's not very descriptive.
+// For instance, the use of a t variable
+// is common in i18n libraries.
+const t = initTRPC.create({
+  /**
+   * @see https://trpc.io/docs/server/data-transformers
+   */
+  // transformer: superjson,
+});
+// Base router and procedure helpers
+export const createTRPCRouter = t.router;
+export const createCallerFactory = t.createCallerFactory;
+export const baseProcedure = t.procedure;
+
+
+// baseProcedure v/s protectedProcedure
+// A baseProcedure is a procedure that does not require authentication.
+// A protectedProcedure is a procedure that requires authentication. - throw error if the user is not logged in.
