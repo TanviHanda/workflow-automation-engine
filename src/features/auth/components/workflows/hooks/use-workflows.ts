acc0eeaed040bcdcfa776808f3228cd9b52cdb6e
TanviHanda <tanvisona2012@gmail.com>
2026-01-31 04:18:25 +0530

    12: workflows pagination

--- src/features/auth/components/workflows/hooks/use-workflows.ts
+++ src/features/auth/components/workflows/hooks/use-workflows.ts
@@ -1,11 +1,13 @@
 import { useMutation, useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
 import { useTRPC } from '@/trpc/client';
 import { toast } from 'sonner';
+import { useWorkflowsParams } from './use-workflows-params';
 
 // hook to fetch all workflows using suspense 
 export const useSuspenseWorkflows = () => {
     const trpc = useTRPC();
-    return useSuspenseQuery(trpc.workflows.getMany.queryOptions())
+    const [params] = useWorkflowsParams();
+    return useSuspenseQuery(trpc.workflows.getMany.queryOptions(params))
 }
 
 // hook to create a new workflow
@@ -17,7 +19,7 @@ export const useCreateWorkflow = () => {
         trpc.workflows.create.mutationOptions({
             onSuccess: (data) => {
                 toast.success(`Workflow "${data.name}" created`);
-                queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions(),
+                queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}),
             );
             },
             onError: (error) => {
