d2af7acb9f992955faa163e56680678afef9ea4f
TanviHanda <tanvisona2012@gmail.com>
2026-03-03 17:26:27 +0530

    18: node execution

---
+++ src/features/executions/components/http-request/executor.ts
@@ -0,0 +1,51 @@
+import type { NodeExecutor } from "@/features/executions/types";
+import { NonRetriableError } from "inngest";
+import ky ,{type Options as kyOptions} from "ky";
+
+type HttpRequestData = {
+    endpoint?: string;
+    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    body?: string;
+}
+
+export const httpRequestExecutor : NodeExecutor<HttpRequestData> = async ({
+    data,
+    nodeId,
+    context,
+    step
+}) => {
+    // TODO: Publish "loading" state for HTTP request
+    if(!data.endpoint){
+        // TODO: Publish "error" state for HTTP request 
+        throw new NonRetriableError("HTTP Request node: NO endpoint configured");
+    }
+   
+    const result = await step.run("http-request",async() => {
+        const endpoint = data.endpoint;
+        const method = data.method || "GET";
+        const options : kyOptions = {method};
+
+        if(["POST","PUT","PATCH"].includes(method)){
+                options.body = data.body;
+        }
+
+        const response = await ky(endpoint,options);
+        const contentType = response.headers.get("content-type");
+        const responseData = contentType?.includes("application/json")
+        ? await response.json()
+        : await response.text();
+        
+        return {
+            ...context,
+            httpResponse: {
+                status: response.status,
+                statusText: response.statusText,
+                data: responseData
+            }
+        }
+    })
+
+    // TODO: Publish "success" state for HTTP request
+
+    return result;
+}
